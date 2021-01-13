1586da1be4d9c84f9ad17f8f0e9f1cfef3193bf9
@BrunoFigueiredo2k <brunofigueiredo@live.nl>
2021-01-13 19:39:59 +0100

    some more tabs logic but not finished

--- js/content/experience.js
+++ js/content/experience.js
@@ -3,6 +3,7 @@ import {generateId} from '../content/projects.js'
 const experienceData = [
     {
         id: generateId(),
+        elemId: "codeerschool",
         company: "De Codeerschool",
         role: "Front-end Developer",
         date: "October 2020 - Now",
@@ -15,10 +16,12 @@ const experienceData = [
             "XML",
             "Android Studio",
             "Adobe Xd"
-        ]
+        ],
+        defaultOpen: true
     },
     {
         id: generateId(),
+        elemId: "kbcreative",
         company: "KB Creative",
         role: "Owner",
         date: "October 2020 - Now",
@@ -30,10 +33,12 @@ const experienceData = [
             "HTML",
             "SCSS",
             "Javascript"
-        ]
+        ],
+        defaultOpen: false
     },
     {
         id: generateId(),
+        elemId: "tsb",
         company: "The Student Broker",
         role: "Front-end Developer",
         date: "April 2020 - Now",
@@ -48,10 +53,12 @@ const experienceData = [
             "jQuery",
             "Bootstrap",
             "PHP"
-        ]
+        ],
+        defaultOpen: false
     },
     {
         id: generateId(),
+        elemId: "gladwell",
         company: "Gladwell Academy",
         role: "Jr. Webmaster",
         date: "February - June 2020",
@@ -64,7 +71,8 @@ const experienceData = [
             "HTML",
             "CSS",
             "Python"
-        ]
+        ],
+        defaultOpen: false
     }
 ]
 
@@ -73,16 +81,21 @@ const displayEducationDOM = () => {
 
     experienceData.map(item => {
         contentExperience.innerHTML += `
-        <h4 class="job-title">${item.company}</h4>
-        <p class="job-date">${item.date}</p>
-        <ul class="job-activities" id="activities-${item.id}">
-            ${populateList('activities')}
-        </ul>
-        <h6 class="job-tech">Used technologies:</h6>
-        <ul class="horizontal-list">
-            ${populateList('tech')}
-        </ul>
-    `
+            <div id="${item.elemId}" class="tabcontent">
+                <h4 class="job-title">${item.company}</h4>
+                <p class="job-date">${item.date}</p>
+                <ul class="job-activities" id="activities-${item.id}">
+                    ${populateList('activities')}
+                </ul>
+                <h6 class="job-tech">Used technologies:</h6>
+                <ul class="horizontal-list">
+                    ${populateList('tech')}
+                </ul>
+        </div>
+        `;
+
+        // If property item default open is true then display that tab, the rest is hidden at first
+        if (item.defaultOpen) { document.getElementById(item.elemId).style.display = "block" }
     })
 
 }
