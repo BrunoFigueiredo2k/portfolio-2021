97a2b67af60c888b4eabee52e52eea9e3e27518a
@BrunoFigueiredo2k <brunofigueiredo@live.nl>
2021-01-15 00:02:56 +0100

    animations added landing page

--- js/components/header.js
+++ js/components/header.js
@@ -8,11 +8,11 @@ export default class Header extends HTMLElement {
         <header>
           <nav class="container">
             <ul class="horizontal-list">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#experience">Experience</a></li>
-                <li><a href="#contact" class="cta-btn">Get in contact</a></li>
+                <li data-aos="fade-down" data-aos-duration="400"><a href="#home">Home</a></li>
+                <li data-aos="fade-down" data-aos-duration="600"><a href="#about">About</a></li>
+                <li data-aos="fade-down" data-aos-duration="800"><a href="#projects">Projects</a></li>
+                <li data-aos="fade-down" data-aos-duration="1000"><a href="#experience">Experience</a></li>
+                <li data-aos="fade-down" data-aos-duration="1200"><a href="#contact" class="cta-btn">Get in contact</a></li>
             </ul>
           </nav>
         </header>
