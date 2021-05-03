1ab7655a5209ea72c4344cca2d06764d3261caf3
@BrunoFigueiredo2k <brunofigueiredo@live.nl>
2021-05-03 20:19:16 +0200

    minor fix link and content

--- js/content/projects.js
+++ js/content/projects.js
@@ -2,59 +2,62 @@ const CLASS_RIGHT_CONTENT = 'right-content';
 const CLASS_LEFT_CONTENT = 'left-content';
 
 export const generateId = () => {
-    return Math.floor(Math.random() * Date.now())
-}
+	return Math.floor(Math.random() * Date.now());
+};
 
 // Projects data
 const projects = [
-    {
-        id: generateId(),
-        title: 'The Student Broker',
-        description: "Website where (international) students can sign up and search for student housing in the Netherlands. The website contains all information students and landlords need to know about our service.",
-        technologies: ['Javascript', 'HTML/CSS', 'PHP', 'SQL', 'jQuery', 'Bootstrap'],
-        linkGithub: null,
-        linkWebsite: 'https://edzijlstra.nl/thestudentbroker/pages/index.php',
-        img: '/images/projects/project-tsb.jpg',
-        directionContent: CLASS_RIGHT_CONTENT
-    },
-    {
-        id: generateId(),
-        title: 'KB Creative',
-        description: "Webdesign & development agency I'm the co-owner of. At KB Creative I'm a front-end developer working closely together with another back-end developer building fullstack web applications",
-        technologies: ['Javascript', 'HTML/CSS'],
-        linkGithub: 'https://github.com/BrunoFigueiredo2k/KB-Creative',
-        linkWebsite: 'https://kbcreative.nl/pages/index.html',
-        img: '/images/projects/project-kbcreative.jpg',
-        directionContent: CLASS_LEFT_CONTENT
-    },
-    {
-        id: generateId(),
-        title: 'Frontend Mentor Challenges',
-        description: "This is a showcase of all the challenges I've done on the platform 'Frontend Mentor' to improve my layout skills.",
-        technologies: ['Javascript', 'HTML/SCSS', 'API'],
-        linkGithub: 'https://github.com/BrunoFigueiredo2k/frontendmentor_challenges_showcase',
-        linkWebsite: 'https://frontendmentor-challenges-showcase-brunofigueiredo2k.vercel.app/',
-        img: '/images/projects/frontend-mentor-challenges.jpg',
-        directionContent: CLASS_RIGHT_CONTENT
-    }
-]
+	{
+		id: generateId(),
+		title: 'The Student Broker',
+		description:
+			'Website where (international) students can sign up and search for student housing in the Netherlands. The website contains all information students and landlords need to know about our service.',
+		technologies: [ 'Javascript', 'HTML/CSS', 'PHP', 'SQL', 'jQuery', 'Bootstrap' ],
+		linkGithub: null,
+		linkWebsite: 'http://thestudentbroker.com/',
+		img: '/images/projects/project-tsb.jpg',
+		directionContent: CLASS_RIGHT_CONTENT
+	},
+	{
+		id: generateId(),
+		title: 'KB Creative',
+		description:
+			"Webdesign & development agency I'm the co-owner of. At KB Creative I'm a front-end developer working closely together with another back-end developer building fullstack web applications",
+		technologies: [ 'Javascript', 'HTML/CSS' ],
+		linkGithub: 'https://github.com/BrunoFigueiredo2k/KB-Creative',
+		linkWebsite: 'https://kbcreative.nl/pages/index.html',
+		img: '/images/projects/project-kbcreative.jpg',
+		directionContent: CLASS_LEFT_CONTENT
+	},
+	{
+		id: generateId(),
+		title: 'Frontend Mentor Challenges',
+		description:
+			"This is a showcase of all the challenges I've done on the platform 'Frontend Mentor' to improve my layout skills.",
+		technologies: [ 'Javascript', 'HTML/SCSS', 'API' ],
+		linkGithub: 'https://github.com/BrunoFigueiredo2k/frontendmentor_challenges_showcase',
+		linkWebsite: 'https://frontendmentor-challenges-showcase-brunofigueiredo2k.vercel.app/',
+		img: '/images/projects/frontend-mentor-challenges.jpg',
+		directionContent: CLASS_RIGHT_CONTENT
+	}
+];
 
 const displayProjectsDOM = () => {
-    // Get element
-    const container = document.getElementById('projects-container');
+	// Get element
+	const container = document.getElementById('projects-container');
 
-    for (var property in projects) {
-        if (projects.hasOwnProperty(property)) {
-            const element = projects[property];
+	for (var property in projects) {
+		if (projects.hasOwnProperty(property)) {
+			const element = projects[property];
 
-            const columnImg = `
+			const columnImg = `
                 <div class="col" data-aos="zoom-in-right" data-aos-duration="1000">
                     <a href="${element.linkWebsite}" target="_blank"
                     rel="noreferrer"><img loading="lazy" src="${element.img}" alt="${element.title}" class="project-img"></a>
                 </div>
             `;
 
-            const columnContent = `
+			const columnContent = `
                 <div class="col project-content ${element.directionContent}" data-aos="zoom-in-left" data-aos-duration="1000">
                     <h3 class="project-title">${element.title}</h3>
                     <div class="project-card">
@@ -70,79 +73,82 @@ const displayProjectsDOM = () => {
                 </div>
             `;
 
-            // Determine direction of content (lef or right) based on class of element
-            if (element.directionContent == CLASS_RIGHT_CONTENT){
-                container.innerHTML += `
+			// Determine direction of content (lef or right) based on class of element
+			if (element.directionContent == CLASS_RIGHT_CONTENT) {
+				container.innerHTML += `
                     <div class="row project">
                         ${columnImg}
                         ${columnContent}
                     </div>
                 `;
-            } else {
-                container.innerHTML += `
+			} else {
+				container.innerHTML += `
                 <div class="row project">
                     ${columnContent}
                     ${columnImg}
                 </div>
             `;
-            }
+			}
 
-            // Populate list of tech
-            const techList = document.getElementById(`project-tech-${element.id}`);
-            element.technologies.forEach(element => {
-                techList.innerHTML += `<li>${element}</li>`;
-            });
+			// Populate list of tech
+			const techList = document.getElementById(`project-tech-${element.id}`);
+			element.technologies.forEach((element) => {
+				techList.innerHTML += `<li>${element}</li>`;
+			});
 
-            // Hide github icon if there is no public repo (null)
-            const linkGithub = document.getElementById(`link-github-${element.id}`);
-            if (element.linkGithub == null){ linkGithub.style.display = 'none'}
-            
-        }
-    }
-}
+			// Hide github icon if there is no public repo (null)
+			const linkGithub = document.getElementById(`link-github-${element.id}`);
+			if (element.linkGithub == null) {
+				linkGithub.style.display = 'none';
+			}
+		}
+	}
+};
 
 /** PROJECTS PAGE */
 const projectsAll = [
-    {
-        title: "Men Spa / Barbershop",
-        description: "Male barbershop/spa that displays the shop's information about service and costs.",
-        img: "../../images/projects/barbershop.jpg",
-        linkWebsite: "https://men-spa.vercel.app/"
-    },
-    {
-        title: "Easybank",
-        description: "Simple landing page challenge from Frontend Mentor to practice some layouts and SCSS.",
-        img: "../../images/projects/easybank-landing-page.jpg",
-        linkWebsite: "https://easybank-landing-page.brunofigueiredo2k.vercel.app/"
-    },
-    {
-        title: "Coffee Shop",
-        description: "Simple landing page challenge from Frontend Mentor to practice some layouts and SCSS.",
-        img: "../../images/projects/project-coffee.jpg",
-        linkWebsite: "https://coffee-shop-tau.vercel.app/"
-    }
-]
+	{
+		title: 'Men Spa / Barbershop',
+		description: "Male barbershop/spa that displays the shop's information about service and costs.",
+		img: '../../images/projects/barbershop.jpg',
+		linkWebsite: 'https://men-spa.vercel.app/'
+	},
+	{
+		title: 'Easybank',
+		description: 'Simple landing page challenge from Frontend Mentor to practice some layouts and SCSS.',
+		img: '../../images/projects/easybank-landing-page.jpg',
+		linkWebsite: 'https://easybank-landing-page.brunofigueiredo2k.vercel.app/'
+	},
+	{
+		title: 'Coffee Shop',
+		description: 'Simple landing page challenge from Frontend Mentor to practice some layouts and SCSS.',
+		img: '../../images/projects/project-coffee.jpg',
+		linkWebsite: 'https://coffee-shop-tau.vercel.app/'
+	}
+];
 
 const displayProjectsPage = () => {
-    // Push data from home page projects to this array of all projects
-    projects.map(project => { projectsAll.push(project) })
-    
-    const row = document.getElementById('projects-row');
-    // Get projects and populate DOM of all projects
-    projectsAll.map(project => {
-        row.innerHTML += `
+	// Push data from home page projects to this array of all projects
+	projects.map((project) => {
+		projectsAll.push(project);
+	});
+
+	const row = document.getElementById('projects-row');
+	// Get projects and populate DOM of all projects
+	projectsAll.map((project) => {
+		row.innerHTML += `
         <div class="project col-4" data-aos="zoom-in-up">
             <div class="content">
                 <img loading="lazy" src="${project.img}"/>
                 <div class="bottom">
                     <h2 class="title">${project.title} <a href="${project.linkWebsite}" target="_blank" rel="noreferrer">
                         <i class="fas fa-external-link-alt sm-icon"></i></a></h2>
-                    <p class="description">${project.description.slice(0, 65) + "..."}</p>
+                    <p class="description">${project.description.slice(0, 65) + '...'}</p>
                 </div>
             </div>
         </div>
         `;
-    })
-}
+	});
+};
 
-export {displayProjectsDOM, displayProjectsPage};
+export { displayProjectsDOM, displayProjectsPage };
